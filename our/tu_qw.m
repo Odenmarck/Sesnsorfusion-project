ea9dd1fcc4136116b47190ef6e166025914b0aec
rasbacks <115184397+rasbacks@users.noreply.github.com>
2025-05-27 15:07:30 +0200

    update

---
+++ our/tu_qw.m
@@ -0,0 +1,23 @@
+function [x, P] = tu_qw(x, P, omega, T, Rw)
+% Performs time update step
+% x: quaternion
+% P: covariance matrix
+% omega: Input gyro values
+% Rw: noise covariance matrix
+F = @(w) T/2 * Somega(w) + eye(4);
+G = @(q) T/2 * Sq(q);
+
+%Motion_model = @(w, q, cov) F(omega)*x + G(x)*mvnrnd(zeros(3,1), Rw).';
+
+x = F(omega)*x; %update x
+P = F(omega)*P*F(omega).' + G(x)*Rw*G(x).';
+[x, P] = mu_normalizeQ(x, P); %Normalizing quaternion
+
+
+
+
+
+
+
+
+ 
